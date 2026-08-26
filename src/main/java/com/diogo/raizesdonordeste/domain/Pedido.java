8402d8654a7e06b8e2709df86afa852c3bbc8462
Diogo Sales <diogosales.ads@gmail.com>
2026-08-26 08:46:33 -0300

    feat: adicionar itens e cálculo do valor total do pedido

--- src/main/java/com/diogo/raizesdonordeste/domain/Pedido.java
+++ src/main/java/com/diogo/raizesdonordeste/domain/Pedido.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -34,5 +36,25 @@ public class Pedido {
     @ManyToOne
     @JoinColumn(name = "id_cliente", nullable = false)
     private Usuario cliente;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    public void adicionaItem(ItemPedido item) {
+        itens.add(item);
+        item.setPedido(this);
+    }
+
+    public void calculaValorTotal() {
+        BigDecimal total = BigDecimal.ZERO;
+        for (ItemPedido item : itens) {
+            BigDecimal subtotal = item
+                    .getPrecoUnitario()
+                    .multiply(BigDecimal.valueOf(item.getQuantidade()));
+
+            total = total.add(subtotal);
+        }
+        this.valorTotal = total;
+    }
 
 }
