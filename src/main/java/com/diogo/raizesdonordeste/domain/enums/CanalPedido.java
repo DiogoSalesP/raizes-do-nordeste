58e5164ee5887fd2a1555023413a7a8f2fcef5dc
Diogo Sales <diogosales.ads@gmail.com>
2026-08-26 08:34:44 -0300

    feat: cria Enum CanalPedido, Enum StatusPedido

---
+++ src/main/java/com/diogo/raizesdonordeste/domain/enums/CanalPedido.java
@@ -0,0 +1,9 @@
+package com.diogo.raizesdonordeste.domain.enums;
+
+public enum CanalPedido {
+    APP,
+    TOTEM,
+    BALCAO,
+    PICKUP,
+    WEB
+}
---
+++ src/main/java/com/diogo/raizesdonordeste/domain/enums/StatusPedido.java
@@ -0,0 +1,11 @@
+package com.diogo.raizesdonordeste.domain.enums;
+
+public enum StatusPedido {
+    AGUARDANDO_PAGAMENTO,
+    PAGAMENTO_APROVADO,
+    PAGAMENTO_RECUSADO,
+    EM_PREPARO,
+    PRONTO_PARA_RETIRADA,
+    FINALIZADO,
+    CANCELADO
+}
