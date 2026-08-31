9464c4966abcd8d764abc7c9893a89190a0ab64f
Diogo Sales <diogosales.ads@gmail.com>
2026-08-31 09:09:21 -0300

    feat: cria PedidoRepository

---
+++ src/main/java/com/diogo/raizesdonordeste/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.diogo.raizesdonordeste.repository;
+
+import com.diogo.raizesdonordeste.domain.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
+}
