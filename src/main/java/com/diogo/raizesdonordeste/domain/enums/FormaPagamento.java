b302d33c3747a349b88eeb1416f33338ee360156
Diogo Sales <diogosales.ads@gmail.com>
2026-09-01 10:28:27 -0300

    feat: cria enums FormaPagamento, StatusPagamento

---
+++ src/main/java/com/diogo/raizesdonordeste/domain/enums/FormaPagamento.java
@@ -0,0 +1,8 @@
+package com.diogo.raizesdonordeste.domain.enums;
+
+public enum FormaPagamento {
+    PIX,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    DINHEIRO
+}
---
+++ src/main/java/com/diogo/raizesdonordeste/domain/enums/StatusPagamento.java
@@ -0,0 +1,9 @@
+package com.diogo.raizesdonordeste.domain.enums;
+
+public enum StatusPagamento {
+    AGUARDANDO,
+    APROVADO,
+    RECUSADO,
+    CANCELADO,
+    ESTORNADO
+}
