4469087eeb2de39f526b6a1d4eca60acde26d690
Diogo Sales <diogosales.ads@gmail.com>
2026-09-02 09:11:05 -0300

    refactor: substitui status do pedido pelo novo fluxo

--- src/main/java/com/diogo/raizesdonordeste/domain/enums/StatusPedido.java
+++ src/main/java/com/diogo/raizesdonordeste/domain/enums/StatusPedido.java
@@ -1,11 +1,8 @@
 package com.diogo.raizesdonordeste.domain.enums;
 
 public enum StatusPedido {
-    AGUARDANDO_PAGAMENTO,
-    PAGAMENTO_APROVADO,
-    PAGAMENTO_RECUSADO,
-    EM_PREPARO,
-    PRONTO_PARA_RETIRADA,
-    FINALIZADO,
+    COZINHA,
+    PRONTO,
+    ENTREGUE,
     CANCELADO
 }
