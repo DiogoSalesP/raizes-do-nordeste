8125f3c8db5a4225552f51f6159dcb8eab379b4a
Diogo Sales <diogosales.ads@gmail.com>
2026-09-10 15:38:41 -0300

    feat: cria EstoqueRepository

---
+++ src/main/java/com/diogo/raizesdonordeste/repository/EstoqueRepository.java
@@ -0,0 +1,10 @@
+package com.diogo.raizesdonordeste.repository;
+
+import com.diogo.raizesdonordeste.domain.Estoque;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EstoqueRepository extends JpaRepository<Estoque, UUID> {
+
+}
